e98199bb24a18efcc8acc84b91e81d0f2398d46a
Ali Can İPEK <alicanipek@outlook.com.tr>
2017-10-18 13:06:54 +0300

    Reverse lookup eklendi.IP lookup değiştirildi.

--- IPLookup.go
+++ IPLookup.go
@@ -8,21 +8,33 @@ import (
 )
 
 func main() {
-	host := flag.String("host", "", "host name")
+	revFlag := flag.Bool("r", false, "reverse lookup")
 
 	flag.Parse()
+	param := flag.Args()
 
-	if *host == "" {
-		flag.Usage()
-		os.Exit(1)
+	if len(param) > 1 {
+		fmt.Printf("You must specify only 1 command line parameter")
 	} else {
-		ips, err := net.LookupIP(*host)
-		if err != nil {
-			fmt.Printf("Could not get IPs: %v\n", err)
-			os.Exit(1)
-		}
-		for _, ip := range ips {
-			fmt.Printf(*host+" IN A %s\n", ip)
+		if *revFlag {
+
+			addresses, err := net.LookupAddr(param[0])
+			if err != nil {
+				fmt.Printf("Could not get Addresses: %v\n", err)
+				os.Exit(1)
+			}
+			for _, address := range addresses {
+				fmt.Printf(param[0]+" IN A %s\n", address)
+			}
+		} else {
+			ips, err := net.LookupIP(param[0])
+			if err != nil {
+				fmt.Printf("Could not get IPs: %v\n", err)
+				os.Exit(1)
+			}
+			for _, ip := range ips {
+				fmt.Printf(param[0]+" IN A %s\n", ip)
+			}
 		}
 	}
 }
